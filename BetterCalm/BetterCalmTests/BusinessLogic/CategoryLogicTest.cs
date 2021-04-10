274eadf4ccd1b3e237583868438b9695a8326b9a
Federico Carbonell <fede_carbonell@hotmail.com>
2021-04-09 22:03:02 -0300

    TestGetAllCategories on Logic layer. Red phase (We have already created the repository and the interface that implements it)

---
+++ BetterCalm/BetterCalmTests/BusinessLogic/CategoryLogicTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogic;
+using BusinessLogicInterface;
+using DataAccessInterface;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Out;
+using Moq;
+
+namespace BetterCalmTests.BusinessLogic
+{
+    [TestClass]
+    public class CategoryLogicTest
+    {
+
+        [TestMethod]
+        public void TestGetAllCategories()
+        {
+            List<Playlist> playlists = new List<Playlist>()
+            {
+                new Playlist()
+                {
+                    Id = 1
+                }
+            };
+            List<Category> categoryToReturn = new List<Category>()
+            {
+                new Category
+                {
+                    Id = 1,
+                    Playlists = playlists
+                }
+            };
+            Mock<ICategoryRepository> mock = new Mock<ICategoryRepository>(MockBehavior.Strict);
+            mock.Setup(m => m.GetAll()).Returns(categoryToReturn);
+
+            mock.VerifyAll();
+        }
+    }
+}
