242b604a5787215d3fdbba65e19dc60c34b6c389
Federico Carbonell <fede_carbonell@hotmail.com>
2021-04-14 21:18:03 -0300

    Adding GetPlaylistByCategory on CategoryLogic test. Teorically red but signature method already exists.

--- BetterCalm/BusinessLogicTests/CategoryLogicTests.cs
+++ BetterCalm/BusinessLogicTests/CategoryLogicTests.cs
@@ -36,5 +36,29 @@ namespace BusinessLogic.Tests
             mock.VerifyAll();
             Assert.AreEqual(categoryToReturn.Count, result.Count);
         }
+
+        [TestMethod]
+        public void TestGetPlaylistByCategoryOk()
+        {
+            List<Playlist> playlists = new List<Playlist>()
+            {
+                new Playlist()
+                {
+                    Id = 1,
+                },
+                new Playlist()
+                {
+                    Id = 2,
+                },
+            };
+            Mock<ICategoryRepository> mock = new Mock<ICategoryRepository>(MockBehavior.Strict);
+            mock.Setup(m => m.GetPlaylistsBy(1)).Returns(playlists);
+            CategoryLogic categoryLogic = new CategoryLogic(mock.Object);
+
+            List<Playlist> result = categoryLogic.GetPlaylistsBy(1);
+
+            mock.VerifyAll();
+            Assert.AreEqual(result.Count, 2);
+        }
     }
 }
