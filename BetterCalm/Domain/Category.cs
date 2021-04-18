6611d495882edb4536d7d8f7da00f4b7d25d0a4c
Federico Carbonell <fede_carbonell@hotmail.com>
2021-04-18 19:41:55 -0300

    Refactor TestGetAllCategoriesEqualError on category repository tests

--- BetterCalm/Domain/Category.cs
+++ BetterCalm/Domain/Category.cs
@@ -7,5 +7,17 @@ namespace Domain
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Playlist> Playlists { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var result = false;
+
+            if (obj is Category category)
+            {
+                result = this.Id == category.Id && this.Name.Equals(category.Name);
+            }
+
+            return result;
+        }
     }
 }
