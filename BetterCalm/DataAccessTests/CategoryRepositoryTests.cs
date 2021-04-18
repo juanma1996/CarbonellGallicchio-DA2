581fc7a035bc069657df25a3091969a5bb2ce467
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-04-18 17:31:30 -0300

    Adding DataAccess test with SqlLite. Red phase.

--- BetterCalm/DataAccessTests/CategoryRepositoryTests.cs
+++ BetterCalm/DataAccessTests/CategoryRepositoryTests.cs
@@ -1,18 +1,66 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DataAccess;
-using System;
+using System.Data.Common;
+using DataAccess.Context;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Text;
+using Domain;
 
 namespace DataAccess.Tests
 {
     [TestClass()]
     public class CategoryRepositoryTests
     {
-        [TestMethod()]
-        public void GetAllTest()
+        private DbConnection connection;
+        private BetterCalmContext context;
+
+        public CategoryRepositoryTests()
         {
-            Assert.Fail();
+            this.connection = new SqliteConnection("Filename=:memory:");
+            this.context = new BetterCalmContext(
+                        new DbContextOptionsBuilder<BetterCalmContext>()
+                        .UseSqlite(connection)
+                        .Options);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.connection.Open();
+            this.context.Database.EnsureCreated();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this.context.Database.EnsureDeleted();
+        }
+
+        [TestMethod()]
+        public void TestGetAllCategories()
+        {
+            List<Playlist> playlists = new List<Playlist>()
+            {
+                new Playlist()
+                {
+                    Id = 1
+                }
+            };
+            List<Category> categoriesToReturn = new List<Category>()
+            {
+                new Category
+                {
+                    Id = 1,
+                    Playlists = playlists
+                }
+            };
+            categoriesToReturn.ForEach(c => context.Add(c));
+            context.SaveChanges();
+
+            CategoryRepository categoryRepository = new CategoryRepository(context);
+            var result = categoryRepository.GetAll();
+
+            Assert.IsTrue(categoriesToReturn.SequenceEqual(result));
         }
     }
 }
