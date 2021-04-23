4d4a38152571bff3fcf0f88787e638335de8f2c3
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-04-23 19:26:12 -0300

    Adding Psychologist migration

---
+++ BetterCalm/DataAccess/Migrations/20210423222356_Psychologist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Psychologist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Psychologists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ConsultationMode = table.Column<string>(nullable: true),
+                    Direction = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Psychologists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Psychologists");
+        }
+    }
+}
