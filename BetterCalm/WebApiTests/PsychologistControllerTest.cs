36f26c1beda702e95a3a9be9458b46a1b03edd9c
Federico Carbonell <fede_carbonell@hotmail.com>
2021-04-24 14:04:01 -0300

    Fixing TestPostPsychologistOk and adding TestPostPsychologistNoProblematics.

--- BetterCalm/WebApiTests/PsychologistControllerTest.cs
+++ BetterCalm/WebApiTests/PsychologistControllerTest.cs
@@ -117,6 +117,25 @@ namespace WebApiTests
                 Direction = "Rio negro",
                 ConsultationMode = "Presencial",
                 CreationDate = new DateTime(2021, 4, 20),
+                Problematics = new List<ProblematicBasicInfoModel>
+                {
+                    new ProblematicBasicInfoModel
+                    {
+                        Name = "Depresion"
+                    },
+                    new ProblematicBasicInfoModel
+                    {
+                        Name = "Ansiedad"
+                    },
+                    new ProblematicBasicInfoModel
+                    {
+                        Name = "Estres"
+                    },
+                    new ProblematicBasicInfoModel
+                    {
+                        Name = "Otros"
+                    },
+                },
             };
 
             Mock<IPsychologistLogicAdapter> mock = new Mock<IPsychologistLogicAdapter>(MockBehavior.Strict);
@@ -186,6 +205,27 @@ namespace WebApiTests
             };
             Mock<IPsychologistLogicAdapter> mock = new Mock<IPsychologistLogicAdapter>(MockBehavior.Strict);
             mock.Setup(m => m.Add(It.IsAny<PsychologistModel>())).Throws(new ArgumentException("Invalid consultation mode"));
+            PsychologistController controller = new PsychologistController(mock.Object);
+
+            var result = controller.Post(psycologistModel);
+            var badResult = result as BadRequestObjectResult;
+
+            mock.VerifyAll();
+            Assert.AreEqual(400, badResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void TestPostPsychologistNoProblematics()
+        {
+            PsychologistModel psycologistModel = new PsychologistModel
+            {
+                Name = "Juan",
+                Direction = "Rio negro",
+                ConsultationMode = "",
+                CreationDate = new DateTime(2021, 4, 20),
+            };
+            Mock<IPsychologistLogicAdapter> mock = new Mock<IPsychologistLogicAdapter>(MockBehavior.Strict);
+            mock.Setup(m => m.Add(It.IsAny<PsychologistModel>())).Throws(new AmountOfProblematicsException());
             PsychologistController controller = new PsychologistController(mock.Object);
 
             var result = controller.Post(psycologistModel);
