099a5fc275401cee3daf1b93b37154f5e4d34f58
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-01 17:27:32 -0300

    Making required Email property in SessionModel

--- BetterCalm/Model/In/SessionModel.cs
+++ BetterCalm/Model/In/SessionModel.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Model.In
 {
     public class SessionModel
     {
+        [Required]
         public string Email { get; set; }
         public string Password { get; set; }
     }
