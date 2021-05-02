0a4cd5838b7e0ff616657b92d8942948c54d2011
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-02 10:56:50 -0300

    Adding ExceptionFilter

---
+++ BetterCalm/ExceptionLogic/ExceptionLogic.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExceptionLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ BetterCalm/WebApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,50 @@
+﻿using AdapterExceptions;
+using ExceptionInterface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace WebApi.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        private readonly IExceptionLogic exceptionLogic;
+
+        public ExceptionFilter(IExceptionLogic exceptionLogic)
+        {
+            this.exceptionLogic = exceptionLogic;
+        }
+        public void OnException(ExceptionContext context)
+        {
+            exceptionLogic.HandleException(context);
+            try
+            {
+                throw context.Exception;
+            }
+            catch (ArgumentInvalidMappingException ex)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 400,
+                    Content = ex.Message
+                };
+            }
+            catch (NullObjectMappingException ex)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = ex.Message
+                };
+            }
+            catch (Exception)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 500,
+                    Content = "Server problems, unexpected error"
+                };
+            }
+        }
+    }
+}
