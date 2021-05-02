5afc096acaceba5de88a247bb0b79f86e0753825
Federico Carbonell <fede_carbonell@hotmail.com>
2021-05-01 23:19:07 -0300

    Add set CreationDate to Psychologist on Add on PsychologistLogic.

--- BetterCalm/BusinessLogic/PsychologistLogic.cs
+++ BetterCalm/BusinessLogic/PsychologistLogic.cs
@@ -24,6 +24,7 @@ namespace BusinessLogic
 
         public Psychologist Add(Psychologist psycologist)
         {
+            psycologist.CreationDate = DateTime.Today;
             return psychologistRepository.Add(psycologist);
         }
 
