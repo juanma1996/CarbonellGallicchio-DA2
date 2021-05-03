cab1825ac6f94f04e99cbca3f7fc9570dffec420
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-02 21:33:29 -0300

    Red phase for TestAdministratorModelWithEmptyName

---
+++ BetterCalm/Validator/PsychologistModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidatorInterface;
+
+namespace Validator
+{
+    public class PsychologistModelValidator : IValidator<PsychologistModel>
+    {
+        public void Validate(PsychologistModel someObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/ValidatorTests/PsychologistModelValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using AdapterExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Validator;
+
+namespace ValidatorTests
+{
+    [TestClass]
+    public class PsychologistModelValidatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestAdministratorModelWithEmptyName()
+        {
+            PsychologistModel psychologist = new PsychologistModel
+            {
+                Name = ""
+            };
+            PsychologistModelValidator validator = new PsychologistModelValidator();
+
+            validator.Validate(psychologist);
+        }
+    }
+}
