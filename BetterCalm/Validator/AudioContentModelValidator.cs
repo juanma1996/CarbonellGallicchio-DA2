ebbd38d372438818bf8a7eb13d802f63c9fc72f3
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-02 21:26:23 -0300

    Red phase for TestAdministratorModelWithEmptyName also adding AudioContentModelValidatorTests and

---
+++ BetterCalm/Validator/AudioContentModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidatorInterface;
+
+namespace Validator
+{
+    public class AudioContentModelValidator : IValidator<AudioContentModel>
+    {
+        public void Validate(AudioContentModel someObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/ValidatorTests/AudioContentModelValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using AdapterExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using Validator;
+
+namespace ValidatorTests
+{
+    [TestClass]
+    public class AudioContentModelValidatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestAdministratorModelWithEmptyName()
+        {
+            AudioContentModel administratorModel = new AudioContentModel
+            {
+                Name = ""
+            };
+            AudioContentModelValidator validator = new AudioContentModelValidator();
+
+            validator.Validate(administratorModel);
+        }
+    }
+}
