b0e2c999262c00d588afd2172adffeeba38006b9
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-03 19:30:40 -0300

    Red phase for PlaylistModelValidator, adding test for empty name

---
+++ BetterCalm/Validator/PlaylistModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidatorInterface;
+
+namespace Validator
+{
+    public class PlaylistModelValidator : IValidator<PlaylistModel>
+    {
+        public void Validate(PlaylistModel someObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/ValidatorTests/PlaylistModelValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using AdapterExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Validator;
+
+namespace ValidatorTests
+{
+    [TestClass]
+    public class PlaylistModelValidatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestNullPlaylist()
+        {
+            PlaylistModel playlist = new PlaylistModel()
+            {
+                Id = 1,
+                Name = "",
+                Description = "One description"
+            };
+            PlaylistModelValidator validator = new PlaylistModelValidator();
+
+            validator.Validate(playlist);
+        }
+    }
+}
