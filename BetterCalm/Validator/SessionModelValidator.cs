98552aa6d2ceb17b83b60dccec72b2cc97dc0435
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-02 21:56:10 -0300

    Red phase: Adding SessionModelValidatorTests

---
+++ BetterCalm/Validator/SessionModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidatorInterface;
+
+namespace Validator
+{
+    public class SessionModelValidator : IValidator<SessionModel>
+    {
+        public void Validate(SessionModel someObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/ValidatorTests/SessionModelValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using AdapterExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Validator;
+
+namespace ValidatorTests
+{
+    [TestClass]
+    public class SessionModelValidatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestSessionModelWithEmptyEmail()
+        {
+            SessionModel sessionModel = new SessionModel
+            {
+                Email = "",
+                Password = "password"
+            };
+            SessionModelValidator validator = new SessionModelValidator();
+
+            validator.Validate(sessionModel);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestSessionModelWithEmptyPassword()
+        {
+            SessionModel sessionModel = new SessionModel
+            {
+                Email = "oneMail",
+                Password = ""
+            };
+            SessionModelValidator validator = new SessionModelValidator();
+
+            validator.Validate(sessionModel);
+        }
+    }
+}
