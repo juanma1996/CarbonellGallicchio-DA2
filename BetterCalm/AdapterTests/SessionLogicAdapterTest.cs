19cdac4005807f57bc83c8ec2c32fc17772175c8
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-03 12:51:58 -0300

    Red phase: adding tests for validate in-models on Create method of SessionLogicAdapter

--- BetterCalm/AdapterTests/SessionLogicAdapterTest.cs
+++ BetterCalm/AdapterTests/SessionLogicAdapterTest.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using Adapter;
 using Adapter.Mapper;
 using Adapter.Mapper.Profiles;
+using AdapterExceptions;
 using AutoMapper;
+using Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using Moq;
+using SessionInterface;
 
 namespace AdapterTests
 {
@@ -15,6 +21,40 @@ namespace AdapterTests
             ModelMapper mapper = new ModelMapper();
             var configuration = new MapperConfiguration(mapper => mapper.AddProfile(new SessionProfile()));
             configuration.AssertConfigurationIsValid();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestPostSessionEmptyEmail()
+        {
+            SessionModel sessionModel = new SessionModel()
+            {
+                Email = "",
+                Password = "onePassword"
+            };
+            Mock<ISessionLogic> mock = new Mock<ISessionLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Add(It.IsAny<string>(), It.IsAny<string>())).Returns(It.IsAny<Session>());
+            ModelMapper mapper = new ModelMapper();
+            SessionLogicAdapter sessionLogicAdapter = new SessionLogicAdapter(mock.Object, mapper);
+
+            var result = sessionLogicAdapter.Add(sessionModel);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestPostSessionEmptyPassword()
+        {
+            SessionModel sessionModel = new SessionModel()
+            {
+                Email = "oneMail@gmail.com",
+                Password = ""
+            };
+            Mock<ISessionLogic> mock = new Mock<ISessionLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Add(It.IsAny<string>(), It.IsAny<string>())).Returns(It.IsAny<Session>());
+            ModelMapper mapper = new ModelMapper();
+            SessionLogicAdapter sessionLogicAdapter = new SessionLogicAdapter(mock.Object, mapper);
+
+            var result = sessionLogicAdapter.Add(sessionModel);
         }
     }
 }
