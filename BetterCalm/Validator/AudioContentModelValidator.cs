404c4cbdf9a4292a029cd66911c117f50771d70c
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-03 20:21:30 -0300

    Refactor AudioContentModelValidator and adding test for check PlaylistModel and CategoryModel

--- BetterCalm/Validator/AudioContentModelValidator.cs
+++ BetterCalm/Validator/AudioContentModelValidator.cs
@@ -6,10 +6,15 @@ namespace Validator
 {
     public class AudioContentModelValidator : IValidator<AudioContentModel>
     {
+
         public void Validate(AudioContentModel audioContentModel)
         {
             if (string.IsNullOrEmpty(audioContentModel.Name))
                 throw new InvalidAttributeException("The audio content name can't be empty");
+            if (audioContentModel.Playlists is null)
+                throw new InvalidAttributeException("The audio content must contain a playlist");
+            if (audioContentModel.Categories is null)
+                throw new InvalidAttributeException("The audio content must contain a category");
         }
     }
 }
