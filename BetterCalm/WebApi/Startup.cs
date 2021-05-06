8ee6b93f9eb0245bbf0b6519d2e44374192a1a1b
Juan Manuel Gallicchio <juan.gallicchio@visitor.upm.com>
2021-05-06 16:28:29 -0300

    Fix merge conflicts

--- BetterCalm/WebApi/Startup.cs
+++ BetterCalm/WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Reflection;
