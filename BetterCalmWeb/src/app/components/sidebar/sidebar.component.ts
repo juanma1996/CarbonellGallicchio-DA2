8bd7a7f711eb1f4a366b80c917796b8143db164d
Federico Carbonell <fede_carbonell@hotmail.com>
2021-05-15 12:20:52 -0300

    [Web] Adding Angular Black Dashboard

---
+++ BetterCalmWeb/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  {
+    path: "/user",
+    title: "Administrator",
+    icon: "icon-single-02",
+    class: ""
+  },
+  {
+    path: "/dashboard",
+    title: "Categories",
+    icon: "icon-headphones",
+    class: ""
+  },
+  {
+    path: "/icons",
+    title: "Consultation",
+    icon: "icon-calendar-60",
+    class: ""
+  },
+];
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+    if (window.innerWidth > 991) {
+      return false;
+    }
+    return true;
+  }
+}
