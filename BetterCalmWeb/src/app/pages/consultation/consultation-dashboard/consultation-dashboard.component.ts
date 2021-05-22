a9a31945f48f737fe4b01594c8a710dbd1e00c4e
Federico Carbonell <fede_carbonell@hotmail.com>
2021-05-21 23:17:33 -0300

    [Web] Adjust of consultation dashboard

--- BetterCalmWeb/src/app/pages/consultation/consultation-dashboard/consultation-dashboard.component.ts
+++ BetterCalmWeb/src/app/pages/consultation/consultation-dashboard/consultation-dashboard.component.ts
@@ -9,7 +9,18 @@ export class ConsultationDashboardComponent implements OnInit {
 
   psychologist = null;
   date: Date = new Date();
-
+  data1 = [
+    { id: '2', itemName: 'Foobar' },
+    { id: '3', itemName: 'Is great' }
+  ];
+  settings1 = {
+    singleSelection: true,
+    text: 'Single Select',
+    enableSearchFilter: false,
+    classes: 'selectpicker btn-primary'
+  };
+  selectedItems1 = [];
+  
   constructor() { }
 
   addConsultation(){
