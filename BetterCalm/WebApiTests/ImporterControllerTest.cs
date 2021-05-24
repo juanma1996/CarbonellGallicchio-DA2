fe0812c1c67be59eba6649391fdcc299723ff115
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-05-23 22:01:10 -0300

    Refactoring tests

--- BetterCalm/WebApiTests/ImporterControllerTest.cs
+++ BetterCalm/WebApiTests/ImporterControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using ImporterLogicInterface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
 using Moq;
 using WebApi.Controllers;
 
@@ -12,12 +13,17 @@ namespace WebApiTests
         [TestMethod]
         public void TestPostImporterOk()
         {
+            ImportModel importModel = new ImportModel()
+            {
+                FilePath = "somePath",
+                ImporterType = "json"
+            };
             string filePath = @"C:\Users\juan.gallicchio\Desktop\ejemplo.json";
             Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
-            mock.Setup(m => m.InstantiateObjectWithKnownInterface(It.IsAny<string>()));
+            mock.Setup(m => m.ImportWithKnownInterface(It.IsAny<ImportModel>()));
             ImporterController controller = new ImporterController(mock.Object);
 
-            var result = controller.Post(filePath);
+            var result = controller.Post(importModel);
             CreatedAtRouteResult createdAtRouteResult = result as CreatedAtRouteResult;
 
             mock.VerifyAll();
