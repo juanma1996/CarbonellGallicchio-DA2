185aef930586b38ce3ed781079ddc775e050cfa3
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-05-30 18:37:08 -0300

    Adding TestAudioContentMapOk red phase

---
+++ BetterCalm/Adapter/Mapper/Profiles/VideoContentProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain;
+using Model.Out;
+
+namespace Adapter.Mapper.Profiles
+{
+    public class VideoContentProfile : Profile
+    {
+        public VideoContentProfile()
+        {
+            CreateMap<VideoContent, VideoContentBasicInfoModel>();
+        }
+    }
+}
---
+++ BetterCalm/AdapterTests/VideoContentLogicAdapterTest.cs
@@ -0,0 +1,19 @@
+﻿using Adapter.Mapper;
+using Adapter.Mapper.Profiles;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdapterTests
+{
+    [TestClass]
+    public class VideoContentLogicAdapterTest
+    {
+        [TestMethod]
+        public void TestAudioContentMapOk()
+        {
+            ModelMapper mapper = new ModelMapper();
+            MapperConfiguration configuration = new MapperConfiguration(mapper => mapper.AddProfile(new VideoContentProfile()));
+            configuration.AssertConfigurationIsValid();
+        }
+    }
+}
