3c826cad8b4e1c5cd763dc22576a8ddb7e082037
Federico Carbonell <fede_carbonell@hotmail.com>
2021-05-29 21:33:17 -0300

    [Web] Minor bug fix on create administrator

--- BetterCalmWeb/src/app/pages/administrator/create-administrator/create-administrator.component.ts
+++ BetterCalmWeb/src/app/pages/administrator/create-administrator/create-administrator.component.ts
@@ -42,7 +42,6 @@ export class CreateAdministratorComponent implements OnInit {
         .subscribe(
           response => {
             this.setSuccess()
-            this.initializeForm();
           },
           catchError => {
             this.setError(catchError);
