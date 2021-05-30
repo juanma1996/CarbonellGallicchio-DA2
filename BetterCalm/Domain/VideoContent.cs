a9822fe1abd44ac24539a9352b4b2671e56c0aa0
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-05-30 18:40:01 -0300

    VideoContent mapper green phase

--- BetterCalm/Domain/VideoContent.cs
+++ BetterCalm/Domain/VideoContent.cs
@@ -1,6 +1,13 @@
-﻿namespace Domain
+﻿using System;
+
+namespace Domain
 {
     public class VideoContent
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string CreatorName { get; set; }
+        public string VideoUrl { get; set; }
     }
 }
