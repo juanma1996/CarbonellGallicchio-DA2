59e448fad1f465ce646fca6ad5b58a7fbf1d5284
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-05-31 19:19:26 -0300

    Adding TestUpdateAudioContentNotExistent red phase

--- BetterCalm/AdapterTests/VideoContentLogicAdapterTest.cs
+++ BetterCalm/AdapterTests/VideoContentLogicAdapterTest.cs
@@ -5,9 +5,11 @@ using AdapterExceptions;
 using AutoMapper;
 using BusinessExceptions;
 using BusinessLogicInterface;
+using Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model.In;
 using Moq;
+using System.Collections.Generic;
 using ValidatorInterface;
 
 namespace AdapterTests
@@ -59,6 +61,48 @@ namespace AdapterTests
                 mockValidator.Object, mockPlaylistModelValidator.Object);
 
             videoContentLogicAdapter.DeleteById(videoContentId);
+
+            mock.VerifyAll();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void TestUpdateAudioContentNotExistent()
+        {
+            int audioContentId = 1;
+            VideoContentModel videoContentModel = new VideoContentModel()
+            {
+                Id = audioContentId,
+                Name = "One audio content",
+                Playlists = new List<PlaylistModel>()
+                {
+                    new PlaylistModel()
+                    {
+                        Id = 1,
+                        Name = "Name",
+                        Description = "Description"
+                    }
+                },
+                Categories = new List<CategoryModel>()
+                {
+                    new CategoryModel()
+                    {
+                        Id = 1,
+                        Name = "Name"
+                    }
+                }
+            };
+            Mock<IPlayableContentLogic> mock = new Mock<IPlayableContentLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Update(It.IsAny<VideoContent>())).Throws(new NullObjectException("Not exist video content"));
+            ModelMapper mapper = new ModelMapper();
+            Mock<IValidator<VideoContentModel>> mockValidator = new Mock<IValidator<VideoContentModel>>(MockBehavior.Strict);
+            mockValidator.Setup(m => m.Validate(It.IsAny<VideoContentModel>()));
+            Mock<IValidator<PlaylistModel>> mockPlaylistModelValidator = new Mock<IValidator<PlaylistModel>>(MockBehavior.Strict);
+            mockPlaylistModelValidator.Setup(m => m.Validate(It.IsAny<PlaylistModel>()));
+            VideoContentLogicAdapter videoContentLogicAdapter = new VideoContentLogicAdapter(mock.Object, mapper,
+                mockValidator.Object, mockPlaylistModelValidator.Object);
+
+            videoContentLogicAdapter.Update(videoContentModel);
 
             mock.VerifyAll();
         }
