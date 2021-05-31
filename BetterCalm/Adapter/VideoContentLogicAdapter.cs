01214915172c52bc14a736e6f8209b37f1ceb200
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-05-31 19:06:08 -0300

    Adding TestGetByIdAudioContentNotExistentId, red phase

---
+++ BetterCalm/Adapter/VideoContentLogicAdapter.cs
@@ -0,0 +1,47 @@
+﻿using Adapter.Mapper;
+using AdapterInterface;
+using AutoMapper;
+using BusinessLogicInterface;
+using Model.In;
+using Model.Out;
+using System;
+using ValidatorInterface;
+
+namespace Adapter
+{
+    public class VideoContentLogicAdapter : IVideoContentLogicAdapter
+    {
+        private readonly IPlayableContentLogic playableContentLogic;
+        private readonly IMapper mapper;
+        private readonly IValidator<VideoContentModel> videoContentModelValidator;
+        private readonly IValidator<PlaylistModel> playlistModelValidator;
+        public VideoContentLogicAdapter(IPlayableContentLogic playableContentLogic, IModelMapper mapper,
+            IValidator<VideoContentModel> videoContentModelValidator, IValidator<PlaylistModel> playlistModelValidator)
+        {
+            this.playableContentLogic = playableContentLogic;
+            this.mapper = mapper.Configure();
+            this.videoContentModelValidator = videoContentModelValidator;
+            this.playlistModelValidator = playlistModelValidator;
+        }
+
+        public VideoContentBasicInfoModel Add(VideoContentModel videoContentModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int videoContentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VideoContentBasicInfoModel GetById(int videoContentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(VideoContentModel videoContentModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BetterCalm/AdapterTests/VideoContentLogicAdapterTest.cs
+++ BetterCalm/AdapterTests/VideoContentLogicAdapterTest.cs
@@ -1,7 +1,14 @@
-﻿using Adapter.Mapper;
+﻿using Adapter;
+using Adapter.Mapper;
 using Adapter.Mapper.Profiles;
+using AdapterExceptions;
 using AutoMapper;
+using BusinessExceptions;
+using BusinessLogicInterface;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using Moq;
+using ValidatorInterface;
 
 namespace AdapterTests
 {
@@ -14,6 +21,26 @@ namespace AdapterTests
             ModelMapper mapper = new ModelMapper();
             MapperConfiguration configuration = new MapperConfiguration(mapper => mapper.AddProfile(new VideoContentProfile()));
             configuration.AssertConfigurationIsValid();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void TestGetByIdAudioContentNotExistentId()
+        {
+            int audioContentId = 1;
+            Mock<IPlayableContentLogic> mock = new Mock<IPlayableContentLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.GetById(It.IsAny<int>())).Throws(new NullObjectException("Not exist audio content"));
+            ModelMapper mapper = new ModelMapper();
+            Mock<IValidator<VideoContentModel>> mockValidator = new Mock<IValidator<VideoContentModel>>(MockBehavior.Strict);
+            mockValidator.Setup(m => m.Validate(It.IsAny<VideoContentModel>()));
+            Mock<IValidator<PlaylistModel>> mockPlaylistModelValidator = new Mock<IValidator<PlaylistModel>>(MockBehavior.Strict);
+            mockPlaylistModelValidator.Setup(m => m.Validate(It.IsAny<PlaylistModel>()));
+            VideoContentLogicAdapter audioContentLogicAdapter = new VideoContentLogicAdapter(mock.Object, mapper,
+                mockValidator.Object, mockPlaylistModelValidator.Object);
+
+            audioContentLogicAdapter.GetById(audioContentId);
+
+            mock.VerifyAll();
         }
     }
 }
