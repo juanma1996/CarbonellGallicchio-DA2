6e107dfe4bf0f71077fceb95134c7c2023feebad
Federico Carbonell <fede_carbonell@hotmail.com>
2021-06-03 19:27:16 -0300

    [Web] Migrate edit administrators table to ngx-datable

--- BetterCalmWeb/src/app/pages/administrator/edit-administrator-dashboard/edit-administrator-dashboard.component.ts
+++ BetterCalmWeb/src/app/pages/administrator/edit-administrator-dashboard/edit-administrator-dashboard.component.ts
@@ -7,38 +7,66 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./edit-administrator-dashboard.component.scss']
 })
 export class EditAdministratorDashboardComponent implements OnInit {
+  entries: number = 10;
+  selected: any[] = [];
+  administrators = [];
+  public isAutenticated: boolean;
 
-  public administrators = [
+  rows: any = [
     {
-      Id: 1,
-      Name: 'SuperAdmin',
-      Email: 'admin@gmail.com',
-      Password: '1234'
+      name: 'SuperAdmin',
+      email: 'admin@gmail.com',
+      password: '1234'
     },
     {
-      Id: 3,
-      Name: 'Fede',
-      Email: 'fede@hotmail.com',
-      Password: 'Fede1'
+      name: 'Fede',
+      email: 'fede@hotmail.com',
+      password: 'Fede1'
     },
     {
-      Id: 4,
-      Name: 'Juan',
-      Email: 'Juan@Juan.com',
-      Password: 'Juan1'
+      name: 'Juan',
+      email: 'Juan@Juan.com',
+      password: 'Juan1'
     },
     {
-      Id: 5,
-      Name: 'fede',
-      Email: 'Fede@fede.com',
-      Password: 'Fede'
+      name: 'fede',
+      email: 'Fede@fede.com',
+      password: 'Fede'
     },
-
   ]
 
   constructor(
-    public toastr: ToastrService
-  ) { }
+    public toastr: ToastrService,
+  ) {
+    this.administrators = this.rows.map((prop, key) => {
+      return {
+        name: prop.name,
+        email: prop.email,
+        password: prop.password,
+        id: prop.id
+      };
+    });
+  }
+
+  entriesChange($event) {
+    this.entries = $event.target.value;
+  }
+
+  filterTable($event) {
+    let val = $event.target.value;
+    this.administrators = this.rows.filter(function (d) {
+      for (var key in d) {
+        if (d[key].toLowerCase().indexOf(val) !== -1) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+
+  consoleButton(value) {
+    console.log(value)
+  }
 
   ngOnInit(): void {
   }
