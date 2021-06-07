1d39e8092124e4c9185dad0daba60205998650cf
Federico Carbonell <fede_carbonell@hotmail.com>
2021-06-07 19:45:13 -0300

    [Web] Deleting unused imports

--- BetterCalmWeb/src/app/pages/video-content/video-content.module.ts
+++ BetterCalmWeb/src/app/pages/video-content/video-content.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VideoContentCardComponent } from './video-content-card/video-content-card.component';
 import { VideoContentListComponent } from './video-content-list/video-content-list.component';
 import { VideoContentDashboardComponent } from './video-content-dashboard/video-content-dashboard.component';
 import { RouterModule } from '@angular/router';
@@ -8,7 +7,7 @@ import { VideoContentRoutes } from './video-content.routing';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
 
 @NgModule({
-  declarations: [VideoContentCardComponent, VideoContentDashboardComponent],
+  declarations: [VideoContentDashboardComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(VideoContentRoutes),
