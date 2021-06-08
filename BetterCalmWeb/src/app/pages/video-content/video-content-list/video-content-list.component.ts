d20aa87cdefb1d443dcf95cfd10b2825998073fa
Federico Carbonell <fede_carbonell@hotmail.com>
2021-06-08 18:37:02 -0300

    [Web] Add isAutenticated on update and delete buttons on video content list

--- BetterCalmWeb/src/app/pages/video-content/video-content-list/video-content-list.component.ts
+++ BetterCalmWeb/src/app/pages/video-content/video-content-list/video-content-list.component.ts
@@ -3,6 +3,7 @@ import { VideoContentBasicInfo } from 'src/app/models/videoContent/video-content
 import { DomSanitizer, SafeResourceUrl, } from '@angular/platform-browser';
 import { VideoContentService } from 'src/app/services/video-content/video-content-service';
 import { ToastService } from 'src/app/common/toast.service';
+import { SessionService } from 'src/app/services/session/session.service';
 
 @Component({
   selector: 'app-video-content-list',
@@ -11,13 +12,16 @@ import { ToastService } from 'src/app/common/toast.service';
 })
 export class VideoContentListComponent implements OnInit {
   @Input() videos: VideoContentBasicInfo[];
+  public isAutenticated: boolean;
 
   constructor(
     private videoContentService: VideoContentService,
+    private sessionService: SessionService,
     public customToastr: ToastService,
   ) { }
 
   ngOnInit(): void {
+    this.isAutenticated = this.sessionService.isAuthenticated();
   }
 
   delete(id) {
