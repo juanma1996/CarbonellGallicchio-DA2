7d836f9cb2957dcb2fdfbfc78733ab67d21517ca
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-09 20:46:19 -0300

    Adding TestGetBonusesOk red phase

---
+++ BetterCalm/WebApi/Controllers/BonusController.cs
@@ -0,0 +1,15 @@
+﻿using BetterCalm.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApi.Controllers
+{
+    [Route("api/bonuses")]
+    public class BonusController : BetterCalmControllerBase
+    {
+        public object Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/WebApiTests/BonusControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Out;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApi.Controllers;
+
+namespace WebApiTests
+{
+    [TestClass]
+    public class BonusControllerTest
+    {
+        [TestMethod]
+        public void TestGetBonusesOk()
+        {
+            BonusBasicInfoModel firstBonusToReturn = new BonusBasicInfoModel()
+            {
+                PacientId = 1,
+                PacientEmail = "juan@gmail.com",
+                ConsultationsQuantity = 6
+            };
+            BonusBasicInfoModel secondBonusToReturn = new BonusBasicInfoModel()
+            {
+                PacientId = 2,
+                PacientEmail = "fede@gmail.com",
+                ConsultationsQuantity = 5
+            };
+            List<BonusBasicInfoModel> bonusesToReturn = new List<BonusBasicInfoModel>() { firstBonusToReturn, secondBonusToReturn };
+            BonusController controller = new BonusController();
+
+            var result = controller.Get();
+            OkObjectResult okResult = result as OkObjectResult;
+            List<BonusBasicInfoModel> bonuses = okResult.Value as List<BonusBasicInfoModel>;
+
+            Assert.AreEqual(bonusesToReturn.Count, bonuses.Count);
+        }
+    }
+}
