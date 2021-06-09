aec80cadaa306cca41ca295634332c3a979c15c0
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-08 21:13:44 -0300

    Adding TestPostConsultationGetCostOk on ConsultationControllerTest red phase

---
+++ BetterCalm/Model/Out/ConsultationBasicInfoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Out
+{
+    public class ConsultationBasicInfoModel
+    {
+        public decimal Cost { get; set; }
+        public PsychologistBasicInfoModel Psychologist { get; set; }
+    }
+}
--- BetterCalm/WebApiTests/ConsultationControllerTest.cs
+++ BetterCalm/WebApiTests/ConsultationControllerTest.cs
@@ -119,5 +119,48 @@ namespace WebApiTests
             Assert.AreEqual(psychologistToReturn.ConsultationMode, psychologistBasicInfoModel.ConsultationMode);
             Assert.AreEqual(psychologistToReturn.Direction, psychologistBasicInfoModel.Direction);
         }
+
+        [TestMethod]
+        public void TestPostConsultationGetCostOk()
+        {
+            int problematicId = 1;
+            decimal cost = 500;
+            ConsultationModel consultationModel = new ConsultationModel
+            {
+                ProblematicId = problematicId,
+                Pacient = new PacientModel
+                {
+                    Name = "Juan",
+                    Surname = "Perez",
+                    BirthDate = new DateTime(1996, 12, 05),
+                    Email = "Juan@gmail.com",
+                    Cellphone = "098342972"
+                }
+            };
+            PsychologistBasicInfoModel psychologistToReturn = new PsychologistBasicInfoModel
+            {
+                Name = "Jorge",
+                ConsultationMode = "Presencial",
+                Direction = "Rio Negro 8156"
+            };
+            ConsultationBasicInfoModel consultationToReturn = new ConsultationBasicInfoModel()
+            {
+                Cost = cost,
+                Psychologist = psychologistToReturn
+            };
+            Mock<IConsultationLogicAdapter> mock = new Mock<IConsultationLogicAdapter>(MockBehavior.Strict);
+            mock.Setup(m => m.Add(It.IsAny<ConsultationModel>())).Returns(psychologistToReturn);
+            ConsultationController controller = new ConsultationController(mock.Object);
+
+            var result = controller.Post(consultationModel);
+            CreatedAtRouteResult okResult = result as CreatedAtRouteResult;
+            ConsultationBasicInfoModel consultationBasicInfoModel = okResult.Value as ConsultationBasicInfoModel;
+
+            mock.VerifyAll();
+            Assert.AreEqual(cost, consultationBasicInfoModel.Cost);
+            Assert.AreEqual(psychologistToReturn.Name, consultationBasicInfoModel.Psychologist.Name);
+            Assert.AreEqual(psychologistToReturn.ConsultationMode, consultationBasicInfoModel.Psychologist.ConsultationMode);
+            Assert.AreEqual(psychologistToReturn.Direction, consultationBasicInfoModel.Psychologist.Direction);
+        }
     }
 }
