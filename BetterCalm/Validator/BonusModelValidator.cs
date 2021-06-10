4e9f82a6abbd52bcf7d203166938d2139f7c3b0b
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-09 23:01:58 -0300

    Adding TestApproveBonusInvalidAmount red phase

---
+++ BetterCalm/Validator/BonusModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Model.In;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidatorInterface;
+
+namespace Validator
+{
+    public class BonusModelValidator : IValidator<BonusModel>
+    {
+        public void Validate(BonusModel bonusModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/ValidatorTests/BonusModelValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using AdapterExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using Validator;
+
+namespace ValidatorTests
+{
+    [TestClass]
+    public class BonusModelValidatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidAttributeException))]
+        public void TestApproveBonusInvalidAmount()
+        {
+            BonusModel bonusModel = new BonusModel()
+            {
+                PacientId = 1,
+                Approved = true,
+                Amount = 5
+            };
+            BonusModelValidator validator = new BonusModelValidator();
+
+            validator.Validate(bonusModel);
+        }
+    }
+}
