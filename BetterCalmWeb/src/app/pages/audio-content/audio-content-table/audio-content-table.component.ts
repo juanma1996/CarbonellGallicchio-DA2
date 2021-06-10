c885df5daff9c26ebe4217e0e47a94988e86e09f
Federico Carbonell <fede_carbonell@hotmail.com>
2021-06-09 23:07:05 -0300

    [Web] Add modal on delete and fix pagination on audio table

--- BetterCalmWeb/src/app/pages/audio-content/audio-content-table/audio-content-table.component.ts
+++ BetterCalmWeb/src/app/pages/audio-content/audio-content-table/audio-content-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SessionService } from 'src/app/services/session/session.service';
 import { AudioContentService } from 'src/app/services/audio-content/audio-content.service';
 import { ToastService } from 'src/app/common/toast.service';
+import { AlertService } from 'src/app/common/alert.service';
 
 @Component({
   selector: 'app-audio-content-table',
@@ -9,20 +10,27 @@ import { ToastService } from 'src/app/common/toast.service';
   styleUrls: ['./audio-content-table.component.scss']
 })
 export class AudioContentTableComponent implements OnInit {
+  entries: number = 10;
   @Input() audios: [];
   public isAutenticated: boolean;
 
   constructor(
     private sessionService: SessionService,
     private audioContentService: AudioContentService,
-    private customToastr: ToastService
+    private customToastr: ToastService,
+    private alert: AlertService
   ) { }
 
   ngOnInit(): void {
     this.isAutenticated = this.sessionService.isAuthenticated();
   }
 
+  deleteModal(id) {
+    this.alert.showAlert("Are you sure you want to delete the audio content?", "I'm sure", this.delete.bind(this, id));
+  }
+
   delete(id) {
+    console.log("id " + id);
     this.audioContentService.delete(id)
       .subscribe(
         response => {
@@ -33,4 +41,9 @@ export class AudioContentTableComponent implements OnInit {
         }
       )
   }
+
+  entriesChange($event) {
+    this.entries = $event.target.value;
+  }
+
 }
