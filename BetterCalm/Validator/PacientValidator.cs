47167c75b508849775dd777f632aeffdcac367c7
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-09 23:44:42 -0300

    Adding TestNullPacient red phase

---
+++ BetterCalm/Validator/PacientValidator.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidatorInterface;
+
+namespace Validator
+{
+    public class PacientValidator : IValidator<Pacient>
+    {
+        public void Validate(Pacient someObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
