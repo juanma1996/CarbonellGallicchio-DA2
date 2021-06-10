a90484e7eaf1eccb502dd59cb82e57ac107ee493
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-09 23:12:36 -0300

    Adding TestGetAudioContentsOk red phase

---
+++ BetterCalm/BusinessLogicTests/BonusLogicTests.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic;
+using DataAccessInterface;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+
+namespace BusinessLogicTests
+{
+    [TestClass]
+    public class BonusLogicTests
+    {
+        [TestMethod]
+        public void TestGetAudioContentsOk()
+        {
+            Pacient firstPacientToReturn = new Pacient()
+            {
+                Id = 1,
+                Email = "juan@email.com",
+                ConsultationsQuantity = 5
+            };
+            Pacient secondPacientToReturn = new Pacient()
+            {
+                Id = 2,
+                Email = "fede@email.com",
+                ConsultationsQuantity = 6
+            };
+            List<Pacient> pacientsToReturn = new List<Pacient>() { firstPacientToReturn, secondPacientToReturn };
+            Mock<IRepository<Pacient>> mock = new Mock<IRepository<Pacient>>(MockBehavior.Strict);
+            mock.Setup(m => m.GetAll(p => p.BonusApproved)).Returns(pacientsToReturn);
+            BonusLogic bonusLogic = new BonusLogic();
+
+            List<Pacient> pacientsWithBonusGenerated = bonusLogic.GetAll();
+
+            mock.VerifyAll();
+            Assert.AreEqual(pacientsToReturn.Count, pacientsWithBonusGenerated.Count);
+        }
+    }
+}
--- BetterCalm/Validator/BonusModelValidator.cs
+++ BetterCalm/Validator/BonusModelValidator.cs
@@ -8,7 +8,7 @@ namespace Validator
     {
         public void Validate(BonusModel bonusModel)
         {
-            if (bonusModel.Amount != 0.15 && bonusModel.Amount != 25 && bonusModel.Amount != 0.50)
+            if (bonusModel.Amount != 0.15 && bonusModel.Amount != 0.25 && bonusModel.Amount != 0.50)
                 throw new InvalidAttributeException("Amount value is invalid. Must be 15, 25 or 50");
         }
     }
