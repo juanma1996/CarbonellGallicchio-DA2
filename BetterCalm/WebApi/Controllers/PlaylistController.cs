63e8358019b72ff818a4d8edf071049e0f2c171f
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-12 16:10:50 -0300

    Implementing Get on PlaylistController for TestGetAllPlaylistOk green phase

--- BetterCalm/WebApi/Controllers/PlaylistController.cs
+++ BetterCalm/WebApi/Controllers/PlaylistController.cs
@@ -1,9 +1,12 @@
 ﻿using AdapterInterface;
 using BetterCalm.WebApi.Controllers;
-using System;
+using Microsoft.AspNetCore.Mvc;
+using Model.Out;
+using System.Collections.Generic;
 
 namespace WebApi.Controllers
 {
+    [Route("api/playlists")]
     public class PlaylistController : BetterCalmControllerBase
     {
         private readonly IPlaylistLogicAdapter playlistLogicAdapter;
@@ -13,9 +16,20 @@ namespace WebApi.Controllers
             this.playlistLogicAdapter = playlistLogicAdapter;
         }
 
-        public object Get()
+        // GET: 
+        /// <summary>
+        /// Obtains all the playlists.
+        /// </summary>
+        /// <remarks>
+        /// Obtains all the information of the playlists on the system.
+        /// </remarks>
+        /// <response code="200">Success. Returns the list of playlists.</response>  
+        /// <response code="500">InternalServerError. Server problems, unexpected error.</response>
+        [HttpGet]
+        public IActionResult Get()
         {
-            throw new NotImplementedException();
+            List<PlaylistBasicInfoModel> playlists = playlistLogicAdapter.GetAll();
+            return Ok(playlists);
         }
     }
 }
