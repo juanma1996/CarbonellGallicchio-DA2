ed2f2af75cafa18941aa6d69132bcbc61ca03696
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-12 16:26:54 -0300

    Adding TestGetAllPlaylists on BusinessLogicTests red phase

---
+++ BetterCalm/BusinessLogic/PlaylistLogic.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogicInterface;
+using DataAccessInterface;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class PlaylistLogic : IPlaylistLogic
+    {
+        private readonly IRepository<Playlist> playlistRepository;
+
+        public PlaylistLogic(IRepository<Playlist> playlistRepository)
+        {
+            this.playlistRepository = playlistRepository;
+        }
+
+        public List<Playlist> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
