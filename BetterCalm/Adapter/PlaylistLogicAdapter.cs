1e5a86adac6cbec523932edf32d6d2d637065df3
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-12 16:20:48 -0300

    Adding TestGetPlaylistsOk on PlaylistLogicAdapterTest red phase

--- BetterCalm/Adapter/PlaylistLogicAdapter.cs
+++ BetterCalm/Adapter/PlaylistLogicAdapter.cs
@@ -1,4 +1,7 @@
-﻿using AdapterInterface;
+﻿using Adapter.Mapper;
+using AdapterInterface;
+using AutoMapper;
+using BusinessLogicInterface;
 using Model.Out;
 using System.Collections.Generic;
 
@@ -6,7 +9,15 @@ namespace Adapter
 {
     public class PlaylistLogicAdapter : IPlaylistLogicAdapter
     {
-        public List<PlaylistBasicInfoModel> GetAll()
+        private readonly IPlaylistLogic playlistLogic;
+        private readonly IMapper mapper;
+
+        public PlaylistLogicAdapter(IPlaylistLogic playlistLogic, IModelMapper mapper)
+        {
+            this.playlistLogic = playlistLogic;
+            this.mapper = mapper.Configure();
+        }
+    public List<PlaylistBasicInfoModel> GetAll()
         {
             throw new System.NotImplementedException();
         }
