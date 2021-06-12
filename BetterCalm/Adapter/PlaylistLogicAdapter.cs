33867df2842424c3e2fa849ad243e550f9db421e
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-12 16:08:02 -0300

    Adding TestGetAllPlaylistOk red phase

---
+++ BetterCalm/Adapter/PlaylistLogicAdapter.cs
@@ -0,0 +1,14 @@
+﻿using AdapterInterface;
+using Model.Out;
+using System.Collections.Generic;
+
+namespace Adapter
+{
+    public class PlaylistLogicAdapter : IPlaylistLogicAdapter
+    {
+        public List<PlaylistBasicInfoModel> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BetterCalm/WebApi/Controllers/PlaylistController.cs
@@ -0,0 +1,21 @@
+﻿using AdapterInterface;
+using BetterCalm.WebApi.Controllers;
+using System;
+
+namespace WebApi.Controllers
+{
+    public class PlaylistController : BetterCalmControllerBase
+    {
+        private readonly IPlaylistLogicAdapter playlistLogicAdapter;
+
+        public PlaylistController(IPlaylistLogicAdapter playlistLogicAdapter)
+        {
+            this.playlistLogicAdapter = playlistLogicAdapter;
+        }
+
+        public object Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
