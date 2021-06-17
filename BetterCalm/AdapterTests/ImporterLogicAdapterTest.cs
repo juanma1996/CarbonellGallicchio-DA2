43838a45ddd4189cb8ca45cf7cd25e4a53902a2a
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-16 23:21:24 -0300

    Adding TestAddImportingOk

---
+++ BetterCalm/AdapterTests/ImporterLogicAdapterTest.cs
@@ -0,0 +1,32 @@
+﻿using Adapter;
+using ImporterLogicInterface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.In;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterTests
+{
+    [TestClass]
+    public class ImporterLogicAdapterTest
+    {
+        [TestMethod]
+        public void TestAddImportingOk()
+        {
+            ImportModel importModel = new ImportModel()
+            {
+                FilePath = "somePath",
+                ImporterType = "json"
+            };
+            Mock<IImporterLogic> mock = new Mock<IImporterLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.ImportWithKnownInterface(It.IsAny<ImportModel>()));
+            ImporterLogicAdapter importerLogicAdapter = new ImporterLogicAdapter(mock.Object);
+
+            importerLogicAdapter.ImportWithKnownInterface(importModel);
+
+            mock.VerifyAll();
+        }
+    }
+}
