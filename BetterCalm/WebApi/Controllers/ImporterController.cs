2705487bc08b9ddcce32299379801ef4d817df69
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-16 23:50:23 -0300

    Adding swagger documentation to ImporterController

--- BetterCalm/WebApi/Controllers/ImporterController.cs
+++ BetterCalm/WebApi/Controllers/ImporterController.cs
@@ -19,6 +19,19 @@ namespace WebApi.Controllers
             this.importerLogicAdapter = importerLogicAdapter;
         }
 
+        // POST: 
+        /// <summary>
+        /// Creates an administrator.
+        /// </summary>
+        /// <remarks>
+        /// Creates the administrator with the information send in the body. An administrator token is required.
+        /// </remarks>
+        /// <response code="201">Created.</response>
+        /// <response code="400">Error. The configurated path is not valid.</response>
+        /// <response code="400">Error. The given path is not valid.</response>
+        /// <response code="401">Unauthorized. Must contain a token to access Api.</response>
+        /// <response code="403">Unauthorized. Forbidden, ask for permission.</response>
+        /// <response code="500">InternalServerError. Server problems, unexpected error.</response>
         [HttpPost]
         public IActionResult Post(ImportModel importModel)
         {
@@ -26,6 +39,18 @@ namespace WebApi.Controllers
             return CreatedAtRoute("", null);
         }
 
+        // GET: 
+        /// <summary>
+        /// Obtains the information of all avaible importers by the configurated path.
+        /// </summary>
+        /// <remarks>
+        /// Obtains the information of all avaible importers by the configurated path. An administrator token is required.
+        /// </remarks>
+        /// <response code="200">Success. Returns the requested object.</response>  
+        /// <response code="400">Error. The configurated path is not valid.</response>
+        /// <response code="401">Unauthorized. Must contain a token to access Api.</response>
+        /// <response code="403">Unauthorized. Forbidden, ask for permission.</response>
+        /// <response code="500">InternalServerError. Server problems, unexpected error.</response>
         [HttpGet]
         public IActionResult Get()
         {
