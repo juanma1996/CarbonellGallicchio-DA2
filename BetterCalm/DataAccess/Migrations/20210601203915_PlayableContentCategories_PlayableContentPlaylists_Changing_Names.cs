eca76ea28126d431bbc968674134f40b642567f2
Juan Manuel Gallicchio <juanma.gallicchio@gmail.com>
2021-06-17 11:53:49 -0300

    Adding ExcludeFromCodeCoverage on migrations

--- BetterCalm/DataAccess/Migrations/20210601203915_PlayableContentCategories_PlayableContentPlaylists_Changing_Names.cs
+++ BetterCalm/DataAccess/Migrations/20210601203915_PlayableContentCategories_PlayableContentPlaylists_Changing_Names.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DataAccess.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class PlayableContentCategories_PlayableContentPlaylists_Changing_Names : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
